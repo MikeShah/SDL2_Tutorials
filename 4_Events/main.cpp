5226ec3bfd465a5d97f2cda95b6403bf6f53b82a
MikeShah <mshah.475@gmail.com>
2021-09-17 14:44:18 -0400

    Events and keyboard

---
+++ 4_Events/main.cpp
@@ -0,0 +1,87 @@
+// On linux compile with:
+// g++ -std=c++17 main.cpp -o prog -lSDL2
+
+// C++ Standard Libraries
+#include <iostream>
+
+// Third-party library
+#include <SDL2/SDL.h>
+
+int main(int argc, char* argv[]){
+
+    // Create a window data type
+    // This pointer will point to the 
+    // window that is allocated from SDL_CreateWindow
+    SDL_Window* window=nullptr;
+
+    // Initialize the video subsystem.
+    // If it returns less than 1, then an
+    // error code will be received.
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        std::cout << "SDL could not be initialized: " <<
+                  SDL_GetError();
+    }else{
+        std::cout << "SDL video system is ready to go\n";
+    }
+
+    // Request a window to be created for our platform
+    // The parameters are for the title, x and y position,
+    // and the width and height of the window.
+    window = SDL_CreateWindow("C++ SDL2 Window",
+            0,
+            2500,
+            640,
+            480,
+            SDL_WINDOW_SHOWN);
+
+    // Infinite loop for our application
+    bool gameIsRunning = true;
+    while(gameIsRunning){
+        SDL_Event event;
+        // Start our event loop
+        while(SDL_PollEvent(&event)){
+            // Handle each specific event
+            if(event.type == SDL_QUIT){
+                gameIsRunning= false;
+            }
+            if(event.type == SDL_MOUSEMOTION){
+                std::cout << "mouse has been moved\n";
+            }
+            if(event.type == SDL_KEYDOWN){
+                std::cout << "a key has been pressed\n";
+                if(event.key.keysym.sym == SDLK_0){
+                    std::cout << "0 was pressed\n";
+                }else{
+                    std::cout << "0 was not pressed\n";
+                }
+            }
+        }
+    }
+
+    // We destroy our window. We are passing in the pointer
+    // that points to the memory allocated by the 
+    // 'SDL_CreateWindow' function. Remember, this is
+    // a 'C-style' API, we don't have destructors.
+    SDL_DestroyWindow(window);
+    
+    // We safely uninitialize SDL2, that is, we are
+    // taking down the subsystems here before we exit
+    // We add a delay in order to see that our window
+    // has successfully popped up.
+    SDL_Delay(3000);
+    // We add a delay in order to see that our window
+    // has successfully popped up.
+    SDL_Delay(3000);
+    // We add a delay in order to see that our window
+    // has successfully popped up.
+    SDL_Delay(3000);
+    // We add a delay in order to see that our window
+    // has successfully popped up.
+    SDL_Delay(3000);
+    // We add a delay in order to see that our window
+    // has successfully popped up.
+    SDL_Delay(3000);
+    // our program.
+    SDL_Quit();
+    return 0;
+}
