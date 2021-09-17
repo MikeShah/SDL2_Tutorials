f1196f7d678a2ff0d9609090187019f21fcd49ae
MikeShah <mshah.475@gmail.com>
2021-09-17 11:48:22 -0400

    Introduction tutorial and first sample

---
+++ 1_Introduction_to_SDL/main.cpp
@@ -0,0 +1,20 @@
+// On linux compile with:
+// g++ -std=c++17 main.cpp -o prog -lSDL2
+
+// C++ Standard Libraries
+#include <iostream>
+
+// Third-party library
+#include <SDL2/SDL.h>
+
+int main(int argc, char* argv[]){
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        std::cout << "SDL could not be initialized: " <<
+                  SDL_GetError();
+    }else{
+        std::cout << "SDL video system is ready to go\n";
+    }
+
+    return 0;
+}
