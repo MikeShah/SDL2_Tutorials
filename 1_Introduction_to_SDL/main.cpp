958ea2360cd44509f366b0514ca5da6a62291a6b
Deton8t <hardeeharris@gmail.com>
2022-12-05 13:05:59 -0500

    Added Mac Compile Text
    Adds text to the top of the first tutorial file to assist Mac users in compiling the file.

--- 1_Introduction_to_SDL/main.cpp
+++ 1_Introduction_to_SDL/main.cpp
@@ -1,18 +1,26 @@
 // On linux compile with:
 // g++ -std=c++17 main.cpp -o prog -lSDL2
 
+// On mac (assuming a brew install of SDL2-- "brew install sdl2" in the command line) you can compile with:
+// clang++ main.cpp -I$HOMEBREW_CELLAR/sdl2/2.26.1/include (includes the headers) -L$HOMEBREW_CELLAR/sdl2/2.26.1/lib -lSDL2-2.0.0 (these two include the library)
+// "2.26.1" would be replaced by the current version of SDL2 you have installed which can be found in the
+// /opt/homebrew/Cellar/sdl2 folder
+
 // C++ Standard Libraries
 #include <iostream>
 
 // Third-party library
 #include <SDL2/SDL.h>
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
-        std::cout << "SDL could not be initialized: " <<
-                  SDL_GetError();
-    }else{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not be initialized: " << SDL_GetError();
+    }
+    else
+    {
         std::cout << "SDL video system is ready to go\n";
     }
 
